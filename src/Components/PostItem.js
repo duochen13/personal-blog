75332091f1b3ca9bb3414fd679508b183110a84c
duochen13 <duochen@umich.edu>
2020-05-21 04:41:44 -0400

    add second posts demo

--- src/Components/PostItem.js
+++ src/Components/PostItem.js
@@ -46,7 +46,25 @@ function PostItem0() {
 function PostItem1() {
     return(
         <div>
-            This is PoteItem 1
+        <div class="sidebar">
+            <a href="#techlead">Techlead</a>
+            <a href="#">Joma</a>
+        </div>
+        <div class="postitem">
+            <h1 id="">Cute SWE Youtubers!</h1>
+            <p>I will introduce some youtubers who currently work in software engineer industry or used to work in tech but get fired because of their youtube channel, also some thoughts after watching some of them</p>
+            <hr />
+            <h3 id="techlead">TechLead</h3>  
+            <p>
+                Ex-google, ex-facebook techlead, a lonely guys keep 
+            </p>
+            <h3 id="joma" > Joma </h3>
+            <p>
+                Techlead's friend, most of his videos have good background music, I think his version of 'a day in the life of a software engineer' video is the bests one among all video in this categories, checked out here, h's video has the bgm that I wanted to search for which song is, also the content is relaxing not like most of videos, which are just showing their fit body, free food at company, which audioence are getting tired of.
+            </p>
+            <h3 id="">Jarvis</h3>
+            <p> My favorite video of him is ''</p>
+        </div>
         </div>
     )    
 }
